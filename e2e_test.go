e29c20264ef2d24a10697801b818376177904eeb
akvlad <akvlad90@gmail.com>
2025-05-12 21:28:13 +0300

    fix tests

--- e2e_test.go
+++ e2e_test.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gigapi/gigapi/config"
-	"github.com/gigapi/gigapi/merge"
-	"github.com/gigapi/gigapi/merge/repository"
-	"github.com/gigapi/gigapi/utils"
+	"github.com/gigapi/gigapi/v2/config"
+	"github.com/gigapi/gigapi/v2/merge"
+	"github.com/gigapi/gigapi/v2/merge/repository"
+	utils2 "github.com/gigapi/gigapi/v2/merge/utils"
+	"github.com/gigapi/gigapi/v2/utils"
+	"io"
 	"os"
 	"runtime/pprof"
+	"strings"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -53,7 +58,7 @@ func TestE2E(t *testing.T) {
 			Secret:        "XXXXXX",
 		},
 	}
-	merge.Init()
+	merge.Init(&api{})
 
 	var data = map[string]any{
 		"timestamp": []int64{},
@@ -90,5 +95,119 @@ func TestE2E(t *testing.T) {
 	}
 	fmt.Printf("%d rows / %v MB written in %v\n", S*N, float64(size*N)/(1024*1024), time.Since(start))
 	fmt.Println("Wating for merge...")
-	time.Sleep(time.Second * 60)
+	time.Sleep(time.Second * 75)
+}
+
+type ParquetData struct {
+	Type             string `json:"type"`
+	ParquetSizeBytes int64  `json:"parquet_size_bytes"`
+	RowCount         int64  `json:"row_count"`
+	MinTime          int64  `json:"min_time"`
+	MaxTime          int64  `json:"max_time"`
+	WalSequence      int64  `json:"wal_sequence"`
+	Files            []File `json:"files"`
+}
+
+type File struct {
+	ID        int    `json:"id"`
+	Path      string `json:"path"`
+	SizeBytes int64  `json:"size_bytes"`
+	RowCount  int64  `json:"row_count"`
+	ChunkTime int64  `json:"chunk_time"`
+	MinTime   int64  `json:"min_time"`
+	MaxTime   int64  `json:"max_time"`
+	Range     string `json:"range"`
+	Type      string `json:"type"`
+}
+
+func TestMetadataFiles(t *testing.T) {
+	// Start CPU profiling
+	stopCPUProfile := startCPUProfile(t)
+	defer stopCPUProfile()
+
+	config.Config = &config.Configuration{
+		Gigapi: config.GigapiConfiguration{
+			Root:          "_testdata",
+			MergeTimeoutS: 10,
+			Secret:        "XXXXXX",
+		},
+	}
+	merge.Init(&api{})
+
+	var data = map[string]any{
+		"timestamp": []int64{},
+		"value":     []float64{},
+		"str":       []string{},
+	}
+	for i := 0; i < 5; i++ {
+		data["timestamp"] = append(data["timestamp"].([]int64), int64(time.Now().UnixNano()))
+		data["value"] = append(data["value"].([]float64), float64(i)/100.0)
+		str := fmt.Sprintf("str%d", i)
+		data["str"] = append(data["str"].([]string), str)
+	}
+
+	db, cancel, err := utils2.ConnectDuckDB("")
+	if err != nil {
+		panic(err)
+	}
+	defer cancel()
+
+	var expectedSize int32 = 0
+
+	checkMetadata := func() int64 {
+		f, err := os.Open(
+			fmt.Sprintf("_testdata/default/test/date=%s/hour=%02d/metadata.json",
+				time.Now().UTC().Format("2006-01-02"), time.Now().UTC().Hour()))
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		data, err := io.ReadAll(f)
+		if err != nil {
+			panic(err)
+		}
+		pData := ParquetData{}
+		json.Unmarshal(data, &pData)
+		var pqts []string
+		for _, f := range pData.Files {
+			pqts = append(pqts, "'"+f.Path+"'")
+		}
+		rows, err := db.Query(fmt.Sprintf("SELECT COUNT(*) FROM read_parquet([%s])", strings.Join(pqts, ",")))
+		if err != nil {
+			panic(err)
+		}
+
+		defer rows.Close()
+		var count int64
+		for rows.Next() {
+			err := rows.Scan(&count)
+			if err != nil {
+				panic(err)
+			}
+		}
+		return count
+	}
+
+	go func() {
+		time.Sleep(time.Second * 5)
+		for {
+			mdSize := checkMetadata()
+			fmt.Printf("Metadata count: %d - %d\n", mdSize, expectedSize)
+			if mdSize != int64(expectedSize) {
+				println("UNEXPECTED metadata count")
+			}
+
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
+	for i := 0; i < 100; i++ {
+		promise := repository.Store("", "test", data)
+		_, err := promise.Get()
+		if err != nil {
+			panic(err)
+		}
+		atomic.AddInt32(&expectedSize, 5)
+		time.Sleep(time.Second)
+	}
 }
